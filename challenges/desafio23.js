a18c23ed9e25ed97dbc25a3d388300832e3a43d7
Thiago Gomes <thiagog.macedom@gmail.com>
2021-09-16 14:27:43 -0300

    corrigindo lint e leve alteracao no 27

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,3 @@
-db.voos.find({ litrosCombustivel: { $lte: 1000 } }, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
+db.voos.find(
+  { litrosCombustivel: { $lte: 1000 } }, { _id: 0, vooId: 1, litrosCombustivel: 1 },
+).limit(1);
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,1 +1,5 @@
-db.voos.find({ litrosCombustivel: { $lte: 600 }, "empresa.nome": { $nin: ["AZUL", "GOL"] } }, { _id: 0, vooId: 1, litrosCombustivel: 1, "empresa.nome": 1 }).limit(1);
+db.voos.find(
+  { litrosCombustivel: { $lte: 600 },
+  "empresa.nome": { $nin: ["AZUL", "GOL"] } },
+  { _id: 0, vooId: 1, litrosCombustivel: 1, "empresa.nome": 1 },
+).limit(1);

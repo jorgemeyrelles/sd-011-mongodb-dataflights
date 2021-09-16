123f246eefe932c8833fc56e07969df8ae3a0896
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-16 18:30:06 -0300

    soluciona desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,13 @@
+db.voos.findOne(
+  { $and:
+    [
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+      { $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] },
+    ],
+  },
+  {
+    vooId: 1,
+    _id: 0,
+  },
+);

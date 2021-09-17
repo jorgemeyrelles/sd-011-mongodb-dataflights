a59efdc2490da3f83d07c7a17d135af241f19579
Lais Simenikim <l.simenikim@gmail.com>
2021-09-16 23:15:37 -0300

    Add challenge 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,5 @@
+db.voos.find({ $and: [{ "aeroportoOrigem.sigla": { $eq: "SBGR" } },
+    { "aeroportoDestino.sigla": { $eq: "KJFK" } }, 
+      { $or: [{ "empresa.nome": { $eq: "DELTA AIRLINES" } },
+        { "empresa.nome": { $eq: "AMERICAN AIRLINES" } },
+      ] }] }, { vooId: 1, _id: 0 }).limit(1);

5a45361ac090f66304cc1e07653d35fccb7bee9b
Lais Simenikim <l.simenikim@gmail.com>
2021-09-16 23:33:27 -0300

    Add challenge 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,11 @@
+db.voos.find({ $and: [{ litrosCombustivel: { $not: { $gt: 600 } } },
+    { $or: [{ "empresa.nome": { $not: { $eq: "GOL" } } },
+    { "empresa.nome": { $not: { $eq: "AZUL" } } },
+    ] },
+    { litrosCombustivel: { $exists: true } },
+    ] },
+    { vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+    _id: 0,
+    }).limit(1);

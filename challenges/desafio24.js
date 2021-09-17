0bd8b375c862d45686d2f43b3551d91cecaf24bd
Diego Figueirêdo <8kball@gmail.com>
2021-09-17 01:00:15 -0300

    Desafio 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,1 +1,1 @@
-db.voos.findOne({ litrosCombustivel: { $gte: 1000 } }, { _id: 0, vooId: 1 });
+db.voos.findOne({ $nor: [{ litrosCombustivel: { $gt: 600 } }, { "empresa.nome": { $in: ["GOL", "AZUL"] } }, { litrosCombustivel: { $exists: false } }] }, { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });

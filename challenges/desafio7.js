8b997ada642522836ae34ea45d703f9c548f1bf6
roberto <robertonnv@gmail.com>
2021-09-22 09:23:54 -0300

    Acertos do lint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,3 +1,3 @@
-db.voos.find({ ano: { $lt:2017 }}).count();
+db.voos.find({ ano: { $lt: 2017 } }).count();
 // https://docs.mongodb.com/manual/reference/operator/query/lt/
 // Referencia menor que para resolução do exercicio
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,3 +1,3 @@
-db.voos.find({ ano: { $gt:2016 }}).count();
+db.voos.find({ ano: { $gt: 2016 } }).count();
 // https://docs.mongodb.com/manual/reference/operator/query/lt/
 // Referencia maior que para resolução do exercicio

d18f227a140fc2c201fba48e2240119b342f9da7
Ana Carolina R Santos <kroline0693@gmail.com>
2021-09-22 19:16:14 -0300

    [FIX]desafio 25, [ADD] desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,24 @@
+// 27 - Retorne a quantidade total de voos de natureza Doméstica que a empresa
+// PASSAREDO possui, via uso de uma nova coleção chamada resumoVoos.
+
+// Conte quantos voos da empresa PASSAREDO cujo campo natureza possua valor igual
+// a Doméstica e crie uma query que insira na coleção resumoVoos um documento com
+// os campos: empresa (nome da empresa) e totalVoosDomesticos 
+// (o total retornado anteriormente).
+// db.voos.count({ $and: [
+//   { "empresa.nome": "PASSAREDO" },
+//   { natureza: "Doméstica" },
+// ] }); // 4187
+
+// db.resumoVoos.insertOne({
+//   empresa: "PASSAREDO",
+//   totalVoosDomesticos: 4187,
+// });
+
+// Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro
+// documento presente na coleção resumoVoos em que a empresa seja PASSAREDO.
+// db.resumoVoos.find(
+//   { empresa: "PASSAREDO" },
+//   { empresa: 1, totalVoosDomesticos: 1, _id: 0 },
+// );
+{ "empresa" : "PASSAREDO", "totalVoosDomesticos" : 4187 }

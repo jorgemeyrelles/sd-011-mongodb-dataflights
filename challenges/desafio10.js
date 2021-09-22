b866933052dd248443b29d1cf689b2f309d60ff6
roberto <robertonnv@gmail.com>
2021-09-22 17:00:46 -0300

    Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,2 +1,7 @@
-db.voos.find({ "empresa.nome": "GOL",ano: 2017 },
-{ _id: 0,vooId: 1,"aeroportOrigem.nome": 1,mes: 1 }).limit(10);
+db.voos.find({ "empresa.nome": "GOL", ano: 2017 },
+{ vooId: 1,
+"aeroportOrigem.nome": 1,
+"aeroportoDestino.nome": 1,
+mes: 1,
+ano: 1,
+_id: 0 }).limit(10);

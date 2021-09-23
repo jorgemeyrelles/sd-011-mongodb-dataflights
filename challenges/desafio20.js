b0f19e6271ed32eb29728352eb4187e91bdcbf1c
roberto <robertonnv@gmail.com>
2021-09-23 12:56:16 -0300

    Desafio

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,4 +1,5 @@
 db.voos.find({ rtk: { $exists: false } },
-    { vooId: 1,
-    _id: 0 }).limit(1);
-    //https://www.mongodbtutorial.org/mongodb-crud/mongodb-exists/
+{ vooId: 1,
+_id: 0 }).limit(1);
+
+//https://www.mongodbtutorial.org/mongodb-crud/mongodb-exists/
--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,5 +1,6 @@
 db.voos.find({ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
-"aeroportoOrigem.sigla": "SBGR", "aeroportoDestino.sigla": "KJFK" },
+"aeroportoOrigem.sigla": "SBGR",
+"aeroportoDestino.sigla": "KJFK" },
 { _id: 0, vooId: 1 }).limit(1);
 // in seleciona documentos onde o valor de um acampo é igual a qualquer valor em uma matriz
 // https://www.mongodbtutorial.org/mongodb-crud/

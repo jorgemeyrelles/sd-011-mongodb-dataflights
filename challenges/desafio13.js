02556f4c1f5649b03c7039ff0cbcff2533ae54b7
roberto <robertonnv@gmail.com>
2021-09-23 10:57:58 -0300

    Desafio13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,3 +1,3 @@
-db.voos.find({ "aeroportoDestino.continente": { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
-// $ in - seleciona documentos onde o valor de um campo é igual a qualquer valor em uma matriz
-// https://docs.mongodb.com/manual/reference/operator/query/in/
+db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
+
+// https://docs.mongodb.com/manual/reference/operator/query/nin/

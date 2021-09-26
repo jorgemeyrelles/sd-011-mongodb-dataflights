4778455ae3f5d00c0b9916184e084be7b5a6afb0
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-26 19:00:11 -0300

    Refactor req-26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,4 @@
-db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $gte: 5, $lte: 10 } });
+db.voos.deleteMany({
+  "empresa.nome": "GOL",
+  "passageiros.pagos": { $gte: 5, $lte: 10 },
+});

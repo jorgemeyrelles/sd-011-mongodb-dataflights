a6e3b7b5f7afbed778e90f5cad32ffef00fc7ccb
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-26 18:52:46 -0300

    Feat: req-24

---
+++ challenges/desafio24.js
@@ -0,0 +1,7 @@
+db.voos.findOne(
+  {
+    litrosCombustivel: { $exists: true, $lte: 600 },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] },
+  },
+  { vooId: true, "empresa.nome": true, litrosCombustivel: true, _id: false },
+);
